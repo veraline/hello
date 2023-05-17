87840f6142e9da69b75130e7e55895b5ce3dfafd
Vera Ndulue <verandulue1@gmail.com>
2023-05-17 22:01:11 +0100

    Hello

---
+++ main.py
@@ -0,0 +1,31 @@
+print("I love pizza ")
+print("Shut the fuck up")
+name = "Ndulue Vera "
+print(name)
+x = 123
+print(x)
+print("hello " + name)
+print(name + str(x))
+
+# Quality, name, sex = 12, "Vera",  "female"
+# print(Quality)
+# print(name)
+# print(sex)
+# Patrick = Phyll = Wisdom = 20
+# print(Patrick)
+# print(Phyll)
+# print(Wisdom)
+# # Typecasting
+# x = 12
+# y =20.9
+# z = "Hello"
+# print(int(y))
+# # User input
+# name = input("What is your name?")
+# print("Hello " + name)
+# age = int(input("How old are you?"))
+# print("you are " + str(age) + " years old")
+
+import math
+pi = 3.14
+print(math.ceil(pi))
